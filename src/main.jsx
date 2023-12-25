c99235c1098f8466d7cd77f73dda227434ddaeaa
SHANG-PENG <peng.shang.gm@gmail.com>
2023-12-25 14:43:48 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { loadStripe } from '@stripe/stripe-js';
 
 import './index.css'
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
