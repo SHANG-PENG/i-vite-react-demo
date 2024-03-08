9b55e8718f427de3bd3f94a6aa1b72c5b6ce767a
SHANG-PENG <peng.shang.gm@gmail.com>
2024-03-08 16:35:33 +0800

    add api project & update index.html

--- public/assets/js/js-form.js
+++ public/assets/js/js-form.js
@@ -8,29 +8,42 @@ function createForm() {
     form.setAttribute("action", "/submit-form"); // 设置表单提交的目标URL  
     form.setAttribute("method", "post"); // 设置表单提交方法  
   
+    var formRowContainer = document.createElement("div"); 
+    formRowContainer.setAttribute("class", "form-group");
+
     // 创建输入字段  
     var nameInput = document.createElement("input");  
     nameInput.setAttribute("type", "text");  
     nameInput.setAttribute("name", "name");  
+    nameInput.setAttribute("autoComplete", "off");  
     nameInput.setAttribute("placeholder", "Your Name");  
   
+    formRowContainer.appendChild(nameInput)
+
+    // 将输入字段和提交按钮添加到表单中  
+    form.appendChild(formRowContainer);  
+
     var emailInput = document.createElement("input");  
     emailInput.setAttribute("type", "email");  
     emailInput.setAttribute("name", "email");  
+    emailInput.setAttribute("autoComplete", "off");  
     emailInput.setAttribute("placeholder", "Your Email");  
-  
+    
+    formRowContainer = document.createElement("div");  
+    formRowContainer.setAttribute("class", "form-group");
+    formRowContainer.appendChild(emailInput)
+    form.appendChild(formRowContainer);  
+
     // 创建提交按钮  
     var submitButton = document.createElement("input");  
     submitButton.setAttribute("type", "submit");  
     submitButton.setAttribute("value", "Submit");  
   
-    // 将输入字段和提交按钮添加到表单中  
-    form.appendChild(nameInput);  
-    form.appendChild(emailInput);  
     form.appendChild(submitButton);  
   
     // 找到要添加表单的容器元素（例如，具有特定ID的元素）  
     var container = document.getElementById("form-container");  
+    container.setAttribute("class", "container");
     if (container) {  
         // 将表单添加到容器中  
         container.appendChild(form);  
